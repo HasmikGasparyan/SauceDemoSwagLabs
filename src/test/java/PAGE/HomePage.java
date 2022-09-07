8d238af990f146223dcf9332c3b128d25a6fd15e
HasmikGasparyan <63506895+HasmikGasparyan@users.noreply.github.com>
2022-09-07 21:36:19 +0400

    Update HomePage.java

--- src/test/java/PAGE/HomePage.java
+++ src/test/java/PAGE/HomePage.java
@@ -10,7 +10,7 @@ public class HomePage {
         this.driver = driver;
     }
 
-    // selecti locatore + filtrelu tarberaki text-e(value-n),
+    // we take the locator of dropdown and the text value of the needed filter option
     public void setFilter() throws InterruptedException{
         basePage = new BasePage(driver);
         basePage.selectElement("css", "[data-test='product_sort_container']", "lohi");
